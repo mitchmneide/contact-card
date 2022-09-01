32dbd610bd2defee3ced8a9cbfb5c7eaaf530b04
MitchelEidegit config --global user.email mitchmneide@gmail.com <mitchmneide@gmail.com>
2022-09-01 16:10:56 -0500

    index db

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,12 +3,14 @@ import './form';
 import './submit';
 // Import css files
 import "../css/index.css"
+import { initdb } from './database';
 import { Tooltip,Toast,Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png'
 window.addEventListener('load',function () {
+    initdb();
     document.getElementById('logo').src= Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src= Dog;
